98a7082d44cee8ef15cdad4d3d0647d585ec7a76
sammie19967 <sammie.barasa@students.jkuat.ac.ke>
2025-04-24 22:06:29 +0300

    Refactor County model: define TownSchema and SubcountySchema, and update CountySchema structure

--- models/County.js
+++ models/County.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 
-const countySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+const TownSchema = new mongoose.Schema({
+  type: String
+}, { _id: false });
+
+const SubcountySchema = new mongoose.Schema({
+  name: String,
+  towns: [String],
+}, { _id: false });
+
+const CountySchema = new mongoose.Schema({
+  id: Number, // This is your custom numeric ID
+  name: String,
+  subcounties: [SubcountySchema],
 });
 
-export default mongoose.models.County || mongoose.model('County', countySchema);
+export default mongoose.models.County || mongoose.model('County', CountySchema);
